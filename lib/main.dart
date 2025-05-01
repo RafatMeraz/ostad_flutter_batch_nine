8b8d7a873c07fa2fafe5088b6c8e4d45dc420d28
RafatMeraz <razhossain201@gmail.com>
2025-05-01 23:15:08 +0600

    Add firebase to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ostad_flutter_batch_nine/app.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const TaskManagerApp());
 }
