1a730c30dce5005ffc120b6e1a77907d29a0d5c8
RafatMeraz <razhossain201@gmail.com>
2025-05-27 23:16:09 +0600

    Add login and sign up screen UI

--- crafty_bay/lib/features/auth/ui/screens/splash_screen.dart
+++ crafty_bay/lib/features/auth/ui/screens/splash_screen.dart
@@ -1,3 +1,5 @@
+import 'package:crafty_bay/features/auth/ui/screens/login_screen.dart';
+import 'package:crafty_bay/features/auth/ui/widgets/app_logo.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -11,7 +13,34 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    _moveToNextScreen();
+  }
+
+  Future<void> _moveToNextScreen() async {
+    await Future.delayed(Duration(seconds: 2));
+    Navigator.pushReplacementNamed(context, LoginScreen.name);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Spacer(),
+              AppLogo(),
+              Spacer(),
+              CircularProgressIndicator(),
+              const SizedBox(height: 16),
+              Text('Version 1.0.0', style: TextStyle(color: Colors.grey)),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
