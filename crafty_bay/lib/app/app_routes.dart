7d70fbed1eed96216623a6b3bd1ae8e62bc547ac
RafatMeraz <razhossain201@gmail.com>
2025-05-29 22:56:41 +0600

    [WIP]: Home screen ui design in progress

--- crafty_bay/lib/app/app_routes.dart
+++ crafty_bay/lib/app/app_routes.dart
@@ -1,6 +1,7 @@
 import 'package:crafty_bay/features/auth/ui/screens/login_screen.dart';
 import 'package:crafty_bay/features/auth/ui/screens/sign_up_screen.dart';
 import 'package:crafty_bay/features/auth/ui/screens/splash_screen.dart';
+import 'package:crafty_bay/features/home/ui/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class AppRoutes {
@@ -13,6 +14,8 @@ class AppRoutes {
       screenWidget = LoginScreen();
     } else if (settings.name == SignUpScreen.name) {
       screenWidget = SignUpScreen();
+    } else if (settings.name == HomeScreen.name) {
+      screenWidget = HomeScreen();
     }
 
     return MaterialPageRoute(builder: (context) => screenWidget);
