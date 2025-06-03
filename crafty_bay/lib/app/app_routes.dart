f98e691ee3cac10c5cc48e1a476b76ab7eb32346
RafatMeraz <razhossain201@gmail.com>
2025-06-03 22:52:47 +0600

    Add product list and wish list screen ui

--- crafty_bay/lib/app/app_routes.dart
+++ crafty_bay/lib/app/app_routes.dart
@@ -2,6 +2,8 @@ import 'package:crafty_bay/features/auth/ui/screens/login_screen.dart';
 import 'package:crafty_bay/features/auth/ui/screens/sign_up_screen.dart';
 import 'package:crafty_bay/features/auth/ui/screens/splash_screen.dart';
 import 'package:crafty_bay/features/common/ui/screens/main_bottom_nav_screen.dart';
+import 'package:crafty_bay/features/product/ui/screens/product_list_screen.dart';
+import 'package:crafty_bay/features/wish_list/ui/screens/wish_list_screen.dart';
 import 'package:flutter/material.dart';
 
 class AppRoutes {
@@ -16,6 +18,9 @@ class AppRoutes {
       screenWidget = SignUpScreen();
     } else if (settings.name == MainBottomNavScreen.name) {
       screenWidget = MainBottomNavScreen();
+    } else if (settings.name == ProductListScreen.name) {
+      final String category = settings.arguments as String;
+      screenWidget = ProductListScreen(category: category);
     }
 
     return MaterialPageRoute(builder: (context) => screenWidget);
